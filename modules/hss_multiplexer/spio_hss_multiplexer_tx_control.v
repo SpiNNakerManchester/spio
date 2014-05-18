01e5a94d7004f85298fb7b6e6de3e4f2e005e2e9
Jonathan Heathcote <mail@jhnet.co.uk>
2014-05-18 11:22:50 +0100

    Fixed dropping of packets by tx_control.

--- modules/hss_multiplexer/spio_hss_multiplexer_tx_control.v
+++ modules/hss_multiplexer/spio_hss_multiplexer_tx_control.v
@@ -7,6 +7,11 @@
  *  * Insert and filter clock correction sequences transparently into the
  *    stream.
  *  * Re-acquire 32-bit word-alignment upon loss of sync.
+ *
+ * Note that since handshakes are usually only restarted after a number of link
+ * errors (e.g. due to disconnection) it is assumed that data is lost on the
+ * link. As a result, the module discards the word it received in the cycle that
+ * a handshake was restarted.
  */
 
 `include "spio_hss_multiplexer_common.h"
@@ -39,7 +44,6 @@ module spio_hss_multiplexer_tx_control #( // The interval at which clock correct
                                         ,   output reg TXRDY_OUT
                                         );
 
-
 // Trigger clock correction at the required interval
 reg [CLOCK_CORRECTION_INTERVAL_BITS-1:0] clock_correction_counter_i;
 always @ (posedge CLK_IN, posedge RESET_IN)
@@ -51,10 +55,22 @@ always @ (posedge CLK_IN, posedge RESET_IN)
 		else
 			clock_correction_counter_i <= 0;
 
-wire send_clock_correction_sequence_i = clock_correction_counter_i == 0;
+wire clock_correction_sequence_due_i = clock_correction_counter_i == 0;
 
 
-// Select the appropriate value to send
+// These signals trigger the transmission of the clock correction sequence and
+// handshake a cycle after TXRDY_OUT was deasserted.
+reg send_clock_correction_sequence_i;
+reg send_handshake_i;
+always @ (posedge CLK_IN, posedge RESET_IN)
+	if (RESET_IN) send_handshake_i <= 1'b1;
+	else          send_handshake_i <= !HANDSHAKE_COMPLETE_IN;
+always @ (posedge CLK_IN, posedge RESET_IN)
+	if (RESET_IN) send_clock_correction_sequence_i <= 1'b0;
+	else          send_clock_correction_sequence_i <= clock_correction_sequence_due_i;
+
+
+// Select the appropriate value to send.
 always @ (posedge CLK_IN, posedge RESET_IN)
 	if (RESET_IN)
 		begin
@@ -67,7 +83,7 @@ always @ (posedge CLK_IN, posedge RESET_IN)
 				TXDATA_OUT    <= {4{`KCH_CLKC}};
 				TXCHARISK_OUT <=  4'b1111;
 			end
-		else if (!HANDSHAKE_COMPLETE_IN)
+		else if (send_handshake_i)
 			begin
 				TXDATA_OUT    <= { `KCH_COMMA
 				                 , `KCH_HANDSHAKE
@@ -90,7 +106,7 @@ always @ (posedge CLK_IN, posedge RESET_IN)
 	if (RESET_IN)
 		TXRDY_OUT <= 1'b0;
 	else
-		TXRDY_OUT <= !send_clock_correction_sequence_i && HANDSHAKE_COMPLETE_IN;
+		TXRDY_OUT <= !clock_correction_sequence_due_i && HANDSHAKE_COMPLETE_IN;
 
 endmodule
 
