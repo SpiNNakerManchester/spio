93657e48299160fcf4c1f4f2ed55d816baa847d7
Luis Plana <plana@cs.man.ac.uk>
2016-08-02 17:51:37 +0100

    - moved to semantic versioning - started at 1.0.0

---
+++ version.h
@@ -0,0 +1,28 @@
+// -------------------------------------------------------------------------
+//
+//  spio version number
+//
+//  semantic versioning system: mayor.minor.patch (M.m.p)
+//
+// -------------------------------------------------------------------------
+// AUTHOR
+//  lap - luis.plana@manchester.ac.uk
+//
+// -------------------------------------------------------------------------
+// DETAILS
+//  Created on       : 02 Aug 2016
+//
+// -------------------------------------------------------------------------
+// COPYRIGHT
+//  Copyright (c) The University of Manchester, 2016.
+//  SpiNNaker Project
+//  Advanced Processor Technologies Group
+//  School of Computer Science
+// -------------------------------------------------------------------------
+`ifndef SPIO_VERSION_H
+`define SPIO_VERSION_H
+
+`define SPIO_VER_STR      "1.0.0"
+`define SPIO_VER_NUM      32'h00010000  // 32'h00MMmmpp
+
+`endif
