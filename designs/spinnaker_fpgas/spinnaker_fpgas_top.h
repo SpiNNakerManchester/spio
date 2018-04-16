60f6eccd0f6dfaeb8c2a4fadc0c4af06ad2d1139
Luis Plana <luis.plana@manchester.ac.uk>
2018-04-16 09:30:11 +0100

    - updated comments.

--- designs/spinnaker_fpgas/spinnaker_fpgas_top.h
+++ designs/spinnaker_fpgas/spinnaker_fpgas_top.h
@@ -7,7 +7,7 @@
 `ifndef SPINNAKER_FPGAS_TOP_H
 `define SPINNAKER_FPGAS_TOP_H
 
-// In the below bit fields, 1=input, 0=output with the LSB corresponding to the
+// In the bit fields below, 1=input, 0=output with the LSB corresponding to the
 // SpiNNaker -> FPGA data pins and FPGA -> SpiNNaker ack pin and the MSB the
 // FPGA -> SpiNNaker data pins and SpiNNaker -> FPGA ack pin.
 localparam LOW_SL    = 2'b10;
@@ -35,9 +35,9 @@ localparam [(3*16*2)-1:0] FPGA_SL_TYPES = { // FPGA2
                                           , HIGH_SL, LOW_SL,  HIGH_SL, LOW_SL
                                           };
 
-// the STAC configuration of the SpiNNaker link senders is different for
-// every SpiNNaker link type (0-5). This array stores the SpiNNkaker link
-// type connected to each FPGA link.
+// the STAC configuration of each link sender depends on the connected
+// SpiNNaker link (0-5). This array stores the SpiNNkaker link
+// number connected to each FPGA link.
 // 3 FPGAs, 16 links per FPGA, 3 bits per link
 localparam [(3*16*3)-1:0] SPIN_LINK_NUM = { // FPGA2
                                             3'd1, 3'd0, 3'd1, 3'd0
